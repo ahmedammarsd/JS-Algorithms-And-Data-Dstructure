1fa873d182673bdc73e4423b88e59284fdfd1cc3
ahmedammarsd <ahmed091999287@gmail.com>
2024-11-14 15:05:30 +0800

    insertion sort

--- Algorithms/sorting_algorithms/insertion_sort.js
+++ Algorithms/sorting_algorithms/insertion_sort.js
@@ -1,1 +1,19 @@
 // Poblem : Given an array of integers, sort the array.
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let numberToInsert = array[i];
+    let j = i - 1;
+    console.log(array);
+    while (j >= 0 && array[j] > numberToInsert) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    }
+    array[j + 1] = numberToInsert;
+  }
+  return array;
+}
+
+console.log(insertionSort([4, 3, 2, 1, 5]));
+
+// Big O = O(n^2)
