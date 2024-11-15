065aab5e903bd41044930f7589cc6e98e5ec6a34
ahmedammarsd <ahmed091999287@gmail.com>
2024-11-15 18:56:42 +0800

    quick sort

---
+++ Algorithms/sorting_algorithms/quick_sort.js
@@ -0,0 +1,25 @@
+// Problem - Given an array of integers, sort the array.
+
+function quickSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+  let pivot = array.length - 1;
+  let rightArray = [];
+  let leftArray = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[pivot] > array[i]) {
+      leftArray.push(array[i]);
+    } else {
+      rightArray.push(array[i]);
+    }
+  }
+
+  return [...quickSort(leftArray), array[pivot], ...quickSort(rightArray)];
+}
+
+console.log(quickSort([2, 3, 8, 7, 5, 4, 9]));
+console.log(quickSort([20, 3, 5, 90, 4, 9]));
+
+// Big O = O(n^2)
