3d106132bd543f251b92e197e26a535f6636e7e4
ahmedammarsd <ahmed091999287@gmail.com>
2024-11-17 15:55:05 +0800

    tower of hanoi the last algorithm in course and some info about algorithms design techniques

---
+++ Algorithms/recap.js
@@ -0,0 +1,25 @@
+/*
+
+ ==========  Algorithm Design Techniques =========
+  1- Bruce Force - 
+      Simple and exhaustive technique that evaluates every possible outcome to find the best solution.
+      EX: Liner Search
+
+  2- Greedy -
+      Choose the best option at the current time , without any consideration for the future    
+      EX: Dijkstra's algorithm, Prim's algorithm and Kruskal's algoritm 
+
+  3- Divide and Conquer -
+      Divide the problem into smaller sub-problems. Each sub-problem is then solved and the partial solution are recombined to determine the overall solution.
+      EX: Binary Search, Quick Sort, Merge Sort, and Tower of Hanoi
+  
+      4- Dynamic Programming - 
+      Divide the problem into smaller sub-problms, Break it down into smaller but overlapping sub problmes. 
+      Store the result and reuse it for the same sub-problems. This is called memoization and is a optimization texhnique that improves the time complexity of your algorithms.
+      EX: fibonacci numbers and climbing staircase   
+  
+      5- Backtracking- 
+      Generate all possible solution. Checkk if the solution satisfies all given constrains and only then you proceed with generating subsequent solutions.
+      if the constraints are not satisfied , backtrack and go on a different path to find the solution.
+      EX: N-Queens problem   
+*/
