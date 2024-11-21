d2b8a6d2d9dad3b3e55802a5ac69b0e0f31cb89a
ahmedammarsd <ahmed091999287@gmail.com>
2024-11-21 16:02:42 +0800

    intorduction or first lectures

--- Data Structures/lectures.js
+++ Data Structures/lectures.js
@@ -1,4 +1,11 @@
 //1 ============== Intorductions  =======================
+/*
+      + A data structure is a way to store and organize data so that it can be used efficiently
+
+      + A data structure is a collection of dta values ,
+        the relationships among them, and the functions or operations that can be applied to that data
+    
+    */
 //1 ============== Intorductions  =======================
 
 //2 ============== Array  =======================
