4968a53e06b30b542a43efd098f9d965bf44cade
ahmedammarsd <ahmed091999287@gmail.com>
2024-11-21 15:54:03 +0800

    data structures - array

---
+++ Data Structures/lectures.js
@@ -0,0 +1,31 @@
+//1 ============== Intorductions  =======================
+//1 ============== Intorductions  =======================
+
+//2 ============== Array  =======================
+/*
+     + An Array is a data structure than can hold a collection of values
+
+     + Arrays can contain a mix of different data types.
+       You can store strings, booleans, numbers or even objects all in tha same array
+
+     + Array are resizable. You don't have to declare the size of an array before creating it
+     
+     + JavaScript arrays are zero-indexed and the insertion order is maintained
+
+     + Array are iterables. They can be used with a for of loop
+
+     
+     methods: push, unshift , shift , pop , map , filter , reduce , slice , concat , split , splice
+
+
+     Array - Big-O time complexity
+       - Insert / Remove from end - O(1)
+       - Inser / Remove from beginnig - O(n)
+       - Access - O(1)
+       - Search - O(n)
+       - Push / Pop - O(1)
+       - shift / unshift / concat / slice / splice - O(n)
+       - forEach / map / filter / reduce - O(n)
+
+   */
+//2 ============== Array  =======================
